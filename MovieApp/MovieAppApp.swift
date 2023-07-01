94918e78abb774fba043f6b5ee97faca142831e8
alpay <calalli1903@gmail.com>
2023-07-01 12:40:17 +0400

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Alpay Calalli on 01.07.23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
